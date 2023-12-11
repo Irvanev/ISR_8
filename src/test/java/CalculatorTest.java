bd24090fb13f5184239e1420c8e96c3bfcf62b8f
vitaly <vitro665@yandex.ru>
2023-12-11 18:44:02 +0300

    Fix

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -3,30 +3,30 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
-    Calculator main = new Calculator();
+    Calculator calculator = new Calculator();
 
     @Test
     void add() {
-        assertEquals(2,main.add(1,1));
+        assertEquals(2,calculator.add(1,1));
         assertEquals(2, calculator.add(1, 1));
     }
 
     @Test
     void dif() {
-        assertEquals(2,main.dif(3,1));
+        assertEquals(2,calculator.dif(3,1));
         assertEquals(0, calculator.dif(1, 1));
     }
 
     @Test
     void div() throws Exception {
-        assertEquals(4,main.div(8,2));
+        assertEquals(4,calculator.div(8,2));
           assertEquals(1, calculator.div(1, 1));
           assertThrows(Exception.class, () -> calculator.div(1, 0));
     }
 
     @Test
     void times() {
-        assertEquals(4,main.times(2,2));
+        assertEquals(4,calculator.times(2,2));
     }
 
     @Test
